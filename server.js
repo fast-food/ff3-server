5b03e6c64b72e977533731660537a6189d979d7b
Omar Aflak <aflakomar@gmail.com>
2018-04-11 13:14:41 +0200

    added food type and menu

--- server.js
+++ server.js
@@ -5,11 +5,24 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({	extended: true }));
-const filepath = __dirname + "/" + "food.json";
+const foodFilePath = __dirname + "/" + "food.json";
+const menuFilePath = __dirname + "/" + "menu.json";
+
+app.get('/menu', function (req, res) {
+    console.log("requested: /menu");
+    fs.readFile(menuFilePath, 'utf8', function (err, data) {
+        if (!err) {
+            res.end(data);
+        }
+        else{
+            res.status(500).send("Could not get data");
+        }
+    });
+})
 
 app.get('/food', function (req, res) {
     console.log("requested: /food");
-    fs.readFile(filepath, 'utf8', function (err, data) {
+    fs.readFile(foodFilePath, 'utf8', function (err, data) {
         if (!err) {
             res.end(data);
         }
@@ -21,7 +34,7 @@ app.get('/food', function (req, res) {
 
 app.get('/sandwich', function (req, res) {
     console.log("requested: /sandwich");
-    fs.readFile(filepath, 'utf8', function (err, data) {
+    fs.readFile(foodFilePath, 'utf8', function (err, data) {
         if (!err) {
             data = JSON.parse(data);
             res.end(JSON.stringify(data.sandwich));
@@ -32,33 +45,9 @@ app.get('/sandwich', function (req, res) {
     });
 })
 
-app.get('/sandwich/:id', function (req, res) {
-    console.log("requested: /sandwich/:id");
-    fs.readFile(filepath, 'utf8', function (err, data) {
-        if (!err) {
-            data = JSON.parse(data);
-            var id = req.params.id;
-            var found = false;
-            for(var i in data.sandwich) {
-               if(data.sandwich[i].id == id) {
-                  res.end(JSON.stringify(data.sandwich[i]));
-                  found = true;
-               }
-            }
-
-            if(!found){
-                res.status(500).send("id does not exist");
-            }
-        }
-        else{
-            res.status(500).send("Could not get data");
-        }
-    });
-})
-
 app.get('/extra', function (req, res) {
     console.log("requested: /extra");
-    fs.readFile(filepath, 'utf8', function (err, data) {
+    fs.readFile(foodFilePath, 'utf8', function (err, data) {
         if(!err) {
             data = JSON.parse(data);
             res.end(JSON.stringify(data.extra));
@@ -71,7 +60,7 @@ app.get('/extra', function (req, res) {
 
 app.get('/drink', function (req, res) {
     console.log("requested: /drink");
-    fs.readFile(filepath, 'utf8', function (err, data) {
+    fs.readFile(foodFilePath, 'utf8', function (err, data) {
         if(!err) {
             data = JSON.parse(data);
             res.end(JSON.stringify(data.drink));
@@ -86,13 +75,13 @@ app.post('/addSandwich', function (req, res) {
     console.log("requested: /addSandwich");
     if(req.body.hasOwnProperty('name')){
         var name = req.body.name;
-        fs.readFile(filepath, 'utf8', function (err, data) {
+        fs.readFile(foodFilePath, 'utf8', function (err, data) {
             if (!err) {
                 data = JSON.parse(data);
                 var index = Object.keys(data.sandwich).length - 1;
                 var id = data.sandwich[index].id + 1;
                 data.sandwich.push({"id":id, "name":name});
-                fs.writeFile(filepath, JSON.stringify(data, null, 4), 'utf8', function (err) {
+                fs.writeFile(foodFilePath, JSON.stringify(data, null, 4), 'utf8', function (err) {
                     if(!err) {
                         res.end();
                     }
@@ -115,13 +104,13 @@ app.post('/addExtra', function (req, res) {
     console.log("requested: /addExtra");
     if(req.body.hasOwnProperty('name')){
         var name = req.body.name;
-        fs.readFile(filepath, 'utf8', function (err, data) {
+        fs.readFile(foodFilePath, 'utf8', function (err, data) {
             if (!err) {
                 data = JSON.parse(data);
                 var index = Object.keys(data.extra).length - 1;
                 var id = data.extra[index].id + 1;
                 data.extra.push({"id":id, "name":name});
-                fs.writeFile(filepath, JSON.stringify(data, null, 4), 'utf8', function (err) {
+                fs.writeFile(foodFilePath, JSON.stringify(data, null, 4), 'utf8', function (err) {
                     if(!err) {
                         res.end();
                     }
@@ -144,13 +133,13 @@ app.post('/addDrink', function (req, res) {
     console.log("requested: /addDrink");
     if(req.body.hasOwnProperty('name')){
         var name = req.body.name;
-        fs.readFile(filepath, 'utf8', function (err, data) {
+        fs.readFile(foodFilePath, 'utf8', function (err, data) {
             if (!err) {
                 data = JSON.parse(data);
                 var index = Object.keys(data.drink).length - 1;
                 var id = data.drink[index].id + 1;
                 data.drink.push({"id":id, "name":name});
-                fs.writeFile(filepath, JSON.stringify(data, null, 4), 'utf8', function (err) {
+                fs.writeFile(foodFilePath, JSON.stringify(data, null, 4), 'utf8', function (err) {
                     if(!err) {
                         res.end();
                     }
