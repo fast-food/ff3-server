06428a3218f0d6e33431b32e4630fce31f4323d2
Omar Aflak <aflakomar@gmail.com>
2018-06-06 16:22:39 +0200

    removed useless code + post order/

--- src/server.js
+++ src/server.js
@@ -8,51 +8,52 @@ app.use(bodyParser.urlencoded({	extended: true }));
 const typeFilePath = __dirname + "/" + "../data/type.json";
 const foodFilePath = __dirname + "/" + "../data/food.json";
 const menuFilePath = __dirname + "/" + "../data/menu.json";
+var type, food, menu;
+
+fs.readFile(typeFilePath, 'utf8', function (err, data) {
+    if (!err) {
+        type = JSON.parse(data);
+    }
+});
+
+fs.readFile(foodFilePath, 'utf8', function (err, data) {
+    if (!err) {
+        food = JSON.parse(data);
+    }
+});
+
+fs.readFile(menuFilePath, 'utf8', function (err, data) {
+    if (!err) {
+        menu = JSON.parse(data);
+    }
+});
 
 app.get('/type', function (req, res) {
     console.log("requested: /type");
-    fs.readFile(typeFilePath, 'utf8', function (err, data) {
-        if (!err) {
-            data = JSON.parse(data);
-            res.end(JSON.stringify(data));
-        }
-        else{
-            res.status(500).send("Could not get data");
-        }
-    });
+    res.end(JSON.stringify(type));
 })
 
 app.get('/menu', function (req, res) {
     console.log("requested: /menu");
-    fs.readFile(menuFilePath, 'utf8', function (err, data) {
-        if(!err){
-            data = JSON.parse(data);
-            res.end(JSON.stringify(data));
-        }
-        else{
-            res.status(500).send("Could not get data");
-        }
-    });
+    res.end(JSON.stringify(menu));
 })
 
 app.get('/food', function (req, res) {
     console.log("requested: /food");
-    fs.readFile(foodFilePath, 'utf8', function (err, data) {
-        if (!err) {
-            data = JSON.parse(data);
-            res.end(JSON.stringify(data));
-        }
-        else{
-            res.status(500).send("Could not get data");
-        }
-    });
+    res.end(JSON.stringify(food));
 })
 
 app.post('/order', function (req, res) {
     console.log("requested: /order");
     if(req.body.hasOwnProperty('order')){
         var order = JSON.parse(req.body.order);
-        console.log(order);
+        console.log("Menu "+order["menu_id"]);
+        for(var i in order["food_ids"]){
+            f = getFoodById(order["food_ids"][i])
+            if(f!=null){
+                console.log("\t"+f["name"]);
+            }
+        }
         res.end();
     }
     else{
@@ -60,67 +61,16 @@ app.post('/order', function (req, res) {
     }
 })
 
-app.post('/food/add', function (req, res) {
-    console.log("requested: /food/add");
-    if(req.body.hasOwnProperty('name') && req.body.hasOwnProperty('type')){
-        var name = req.body.name;
-        var type = parseInt(req.body.type);
-        fs.readFile(foodFilePath, 'utf8', function (err, data) {
-            if (!err) {
-                data = JSON.parse(data);
-                var index = Object.keys(data).length - 1;
-                var id = data[index].id + 1;
-                data.push({"id":id, "type":type, "name":name});
-                fs.writeFile(foodFilePath, JSON.stringify(data, null, 4), 'utf8', function (err) {
-                    if(!err){
-                        res.end();
-                    }
-                    else{
-                        res.status(500).send("Could not write data");
-                    }
-                });
-            }
-            else{
-                res.status(500).send("Could not read data");
-            }
-        });
-    }
-    else{
-        res.status(500).send("You must specify the following fields: \"name\", \"type\"");
-    }
-})
-
-app.post('/menu/add', function (req, res) {
-    console.log("requested: /menu/add");
-    if(req.body.hasOwnProperty('price') && req.body.hasOwnProperty('food_type')){
-        var price = parseFloat(req.body.price);
-        var foodType = JSON.parse(req.body.food_type);
-        fs.readFile(menuFilePath, 'utf8', function (err, data) {
-            if (!err) {
-                data = JSON.parse(data);
-                var index = Object.keys(data).length - 1;
-                var id = data[index].id + 1;
-                data.push({"id":id, "price":price, "food_type":foodType});
-                fs.writeFile(menuFilePath, JSON.stringify(data, null, 4), 'utf8', function (err) {
-                    if(!err) {
-                        res.end();
-                    }
-                    else{
-                        res.status(500).send("Could not write data");
-                    }
-                });
-            }
-            else{
-                res.status(500).send("Could not read data");
-            }
-        });
-    }
-    else{
-        res.status(500).send("You must specify the following fields: \"name\", \"food_type[]\"");
-    }
-})
-
 var server = app.listen(8080, function () {
     var port = server.address().port
     console.log("Server listening at http://127.0.0.1:%s", port)
 })
+
+function getFoodById(id){
+    for(var i in food){
+        if(food[i]["id"]==id){
+            return food[i]
+        }
+    }
+    return null
+}
